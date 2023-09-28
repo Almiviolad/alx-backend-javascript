652dde205d0ba9145ddc1b7d88c4edfbc20a3e64
root <mykelaloh@gmail.com>
2023-09-28 11:16:51 -0700

    taak2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-expressions */
-export default function handleResponseFromAPI(promise) {
-  promise.then((obj) => { obj; })
+export default function handleResponseFromAPI(promise){
+  return promise.then((obj) => { obj; })
     .catch((Error) => { Error(); })
     .finally(() => { console.log('Got a response from the API'); });
 }
