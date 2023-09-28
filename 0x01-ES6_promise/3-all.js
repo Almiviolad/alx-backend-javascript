a3ab555ceeed31aedb49c44d4f549b50f4f97098
root <mykelaloh@gmail.com>
2023-09-28 14:20:09 -0700

    returned the promise in task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,8 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
-
 export default function handleProfileSignup() {
   const promises = [uploadPhoto(), createUser()];
-  Promise.all(promises)
+  return Promise.all(promises)
     .then((results) => {
       const [photoResult, userResult] = results;
       console.log(photoResult.body, userResult.firstName, userResult.lastName);
