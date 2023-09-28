72bc4d2eb0bafa201c18f45f9e0ca42f8bc5f966
root <mykelaloh@gmail.com>
2023-09-28 14:40:39 -0700

    task 3 obeyed eslint corrections

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,4 +1,5 @@
 import { uploadPhoto, createUser } from './utils';
+
 export default function handleProfileSignup() {
   const promises = [uploadPhoto(), createUser()];
   return Promise.all(promises)
--- 0x01-ES6_promise/3-main.js
+++ 0x01-ES6_promise/3-main.js
@@ -1,3 +1,4 @@
 import handleProfileSignup from "./3-all";
 
 handleProfileSignup();
+console.log(handleProfileSignup() instanceof Promise)
