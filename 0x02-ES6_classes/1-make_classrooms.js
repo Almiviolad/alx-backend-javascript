e9257140feb31587c426335754411cc4423f656e
root <mykelaloh@gmail.com>
2023-10-01 05:18:57 -0700

    task 1: correcter the curly braces i used for array

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,6 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const room19 = new ClassRoom(19);
-  const room20 = new ClassRoom(20);
-  const room34 = new ClassRoom(34);
-  const array = { room19, room20, room34 };
-  return array;
+  const rooms = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+  return rooms;
 }
