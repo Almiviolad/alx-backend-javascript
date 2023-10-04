6e8e3854d108add4308b01687d0399ead37e2db7
root <mykelaloh@gmail.com>
2023-10-04 03:25:20 -0700

    task 100: error shoukd be thrown if the value +1 is greater

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -4,10 +4,11 @@ export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
     weakMap.set(endpoint, 1);
   } else {
-      const val = weakMap.get(endpoint);
-      if (val >= 5) {
-          throw Error('Endpoint load is high');}
-      weakMap.set(endpoint, val + 1);
+    const val = weakMap.get(endpoint);
+    weakMap.set(endpoint, val + 1);
+    if (val + 1 >= 5) {
+      throw Error('Endpoint load is high');
+    }
   }
 
   return weakMap;
