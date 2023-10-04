199198a23dd8a0e433a5be03ac91538ed4c8d541
root <mykelaloh@gmail.com>
2023-10-04 02:21:23 -0700

    updated the file for error thrown

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -4,11 +4,10 @@ export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
     weakMap.set(endpoint, 1);
   } else {
-    const val = weakMap.get(endpoint);
-    weakMap.set(endpoint, val + 1);
-    if (val >= 5) {
-      throw Error('Endpoint load is high');
-    }
+      const val = weakMap.get(endpoint);
+      if (val >= 5) {
+          throw Error('Endpoint load is high');}
+      weakMap.set(endpoint, val + 1);
   }
 
   return weakMap;
