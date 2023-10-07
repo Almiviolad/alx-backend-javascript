b6b0365e48d1cb5ce06ff1290e437dc7c93f805a
root <mykelaloh@gmail.com>
2023-10-07 06:03:50 -0700

    task 100

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable class-methods-use-this */
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super();
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+    this._range = range;
+  }
+
+  cloneCar() {
+    return new Car();
+  }
+}
