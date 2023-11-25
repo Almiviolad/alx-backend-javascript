bdf079f4a238fc49ccdbd02794f88615327f1cc6
root <mykelaloh@gmail.com>
2023-11-25 09:09:31 -0800

    created file 1 and addes right content to it

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,18 +1,7 @@
-function calculateNumber(type, a, b) {
+function calculateNumber(a, b) {
     /*round a and b and return the result of mat opertion done on the tow numbers*/
     a = Math.round(a);
     b = Math.round(b);
-    if (type === 'SUM') {
-        return (a + b);
-    }
-    else if (type === 'SUBTRACT') {
-        return (a - b);
-    }
-    else if (type === 'DIVIDE') {
-        if (b === 0) {
-            return "Error";
-        }
-        return (a / b);
-    }
-}
-module.exports = calculateNumber;
+    return (a + b);
+};
+module.exports = calculateNumber
